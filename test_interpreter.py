f0af2d1b4c8f2a1168c81f040a06c66a6b7fcfa1
ggay <ggay@student.42.fr>
2024-10-14 15:41:26 +0200

    FEAT: added simple test for tokenisation and interpreter

---
+++ test_interpreter.py
@@ -0,0 +1,46 @@
+from Token import tokenize
+from Expression import Parser
+from Interpreter import Interpreter
+import unittest
+
+
+class TestInterpreter(unittest.TestCase):
+    def my_set_up(self, cmd: str):
+        token_list = tokenize(cmd)
+        parser = Parser(token_list)
+        ast = parser.generate()
+        return ast
+
+    def test_addition(self):
+        ast = self.my_set_up("5 + 2")
+        interpreter = Interpreter(ast)
+        self.assertEqual("7", interpreter.interpret())
+
+    def test_subtraction(self):
+        ast = self.my_set_up("44 - 2")
+        interpreter = Interpreter(ast)
+        self.assertEqual("42", interpreter.interpret())
+
+    def test_multiplication(self):
+        ast = self.my_set_up("21 * 2")
+        interpreter = Interpreter(ast)
+        self.assertEqual("42", interpreter.interpret())
+
+    def test_division(self):
+        ast = self.my_set_up("84 / 2")
+        interpreter = Interpreter(ast)
+        self.assertEqual("42.0", interpreter.interpret())
+
+    def test_precedence(self):
+        ast = self.my_set_up("5 + 2 * 21")
+        interpreter = Interpreter(ast)
+        self.assertEqual("47", interpreter.interpret())
+
+    def test_parenthesis(self):
+        ast = self.my_set_up("(5 + 16) * 2")
+        interpreter = Interpreter(ast)
+        self.assertEqual("42", interpreter.interpret())
+
+
+if __name__ == '__main__':
+    unittest.main()
